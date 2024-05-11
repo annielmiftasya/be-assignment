1d74baa2a3a1217cb4b75f0833503c3e259cbc95
annielmiftasya <annnielmiftasya@gmail.com>
2024-05-12 02:14:10 +0700

    fixing transaction

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -67,27 +67,28 @@ async function withdrawTransaction(request, reply) {
 }
 
 async function getAccountTransactions(req, res) {
-  const { accountId } = req.params;
+    const { accountId } = req.params;
 
-  try {
-    
-    const accountIdInt = parseInt(accountId);
-    
-    // Check if accountIdInt is a valid integer
-    if (isNaN(accountIdInt)) {
-      throw new Error('Invalid accountId');
+    try {
+
+        const accountIdInt = parseInt(accountId);
+
+        // Check if accountIdInt is a valid integer
+        if (isNaN(accountIdInt)) {
+            throw new Error('Invalid accountId');
+        }
+
+        const transactions = await prisma.transaction.findMany({
+            where: { accountId: accountIdInt },
+        });
+
+        res.send(transactions);
+    } catch (error) {
+        console.error('Error fetching account transactions:', error);
+        res.status(500).send({ error: 'Internal server error' });
     }
-    
-    const transactions = await prisma.transaction.findMany({
-      where: { accountId: accountIdInt },
-    });
-
-    res.send(transactions); 
-  } catch (error) {
-    console.error('Error fetching account transactions:', error);
-    res.status(500).send({ error: 'Internal server error' });
-  }
 }
+
 
 
 module.exports = { sendTransaction, withdrawTransaction, getAccountTransactions };
